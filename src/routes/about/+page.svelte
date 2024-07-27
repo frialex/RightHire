5596d5ac64ce5a6f881555f4da6473b9529abaea
frialex <fri.b.alex@gmail.com>
2024-07-27 13:16:57 -0400

    improvements

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,22 +3,53 @@
 	<meta name="description" content="About this app" />
 </svelte:head>
 
+
+
 <div class="text-column">
 	<p>Record camera for interview questions</p>
 	
 	<video autoplay></video>
+  <button on:click={startRecord}>Start</button>
+  <button on:click={stopRecording}>Stop</button>
 </div>
 
 
 
-<script>
-  navigator.mediaDevices
-    .getUserMedia({ video: true, audio: true })
-    .then((localMediaStream) => {
+<script lang="ts">
+  import { browser } from "$app/environment";
+
+  let recorder: MediaRecorder;
+
+  browser && navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: "user" }, audio: true })
+    .then((stream) => {
       const video = document.querySelector("video");
-      video.srcObject = localMediaStream;
+      if(video) video.srcObject = stream;
+
+      recorder = new MediaRecorder(stream);
+      recorder.ondataavailable = (e)=>{
+        let blob = e.data;
+        let type = blob.type;
+        //TODO: post blob to api endpoint to be saved
+
+        let url = URL.createObjectURL(blob);
+        playBlobVideo(url)
+        debugger;
+
+      }
+
     })
     .catch((error) => {
       console.log("Rejected!", error);
     });
+
+    
+    function playBlobVideo(url){
+      //TODO;
+      //https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/dataavailable_event
+        
+    }
+
+    function startRecord(){    recorder?.start();  }
+    function stopRecording(){  recorder.stop();    }
 </script>
